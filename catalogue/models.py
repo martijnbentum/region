5da0859aa6b50d4ba8ac872a532411056bd33b15
Martijn Bentum <bentummartijn@gmail.com>
2019-11-04 16:31:48 +0100

    added __str__ to models

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django_countries.fields import CountryField
 
+
 class Location(models.Model):
 	STATUS = [('F','fiction'), ('NF','non-fiction')]
 	status = models.CharField(max_length=2,choices=STATUS)
@@ -11,7 +12,7 @@ class Location(models.Model):
 	province = models.CharField(max_length=100)
 
 	def __str__(self):
-		return self.status
+		return self.country.name
 
 class LocationType(models.Model):
 	pass
@@ -29,6 +30,10 @@ class Person(models.Model):
 	date_of_death = models.DateField()
 	residence = models.ForeignKey(Location, on_delete=models.CASCADE) # multiple residences?? duplicate of PersonLocationRelation??
 	notes = models.TextField() # one to many
+	
+	def __str__(self):
+		return self.first_name + ' ' + self.last_name
+
 
 class PersonLocationRelation(models.Model):
 	person = models.ForeignKey(Person, on_delete=models.CASCADE)
@@ -56,12 +61,18 @@ class PersonWorkRelation(models.Model):
 class Genre(models.Model):
 	name = models.CharField(max_length=100);
 	description = models.TextField()
+	
+	def __str__(self):
+		return self.name
 
 class Text(models.Model):
 	title = models.CharField(max_length=300)
 	language = models.CharField(max_length=100)
 	genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
 	upload = models.FileField(upload_to='texts/') # ?
+
+	def __str__(self):
+		return self.title
 
 
 class TextTextRelation(models.Model):
@@ -76,6 +87,9 @@ class Illustration(models.Model):
 	context = models.TextField()
 	illustration_format = '' # ... | ...
 	upload = models.ImageField(upload_to='illustrations/') # ?
+	
+	def __str__(self):
+		return self.caption
 
 class IllustrationCategory(models.Model):
 	category = models.CharField(max_length=100)
@@ -95,20 +109,33 @@ class Periodical(models.Model):
 	end_date = models.DateField()
 	location = models.ForeignKey(Location, on_delete=models.CASCADE)
 
+	def __str__(self):
+		return self.title
+
 class Audience(models.Model): # only usefull for periodical not book?
 	name = models.CharField(max_length=100)
 	description = models.TextField()
+
+	def __str__(self):
+		return self.name
 
 
 class Book(models.Model):
 	title = models.CharField(max_length=300) # duplicate -> text
 	language = models.CharField(max_length=100) # duplicate -> text
+
+	def __str__(self):
+		return self.title
 	
 class Publisher(models.Model):
 	name = models.CharField(max_length=300)
 	start_date = models.DateField()
 	end_date = models.DateField()
 	notes = models.TextField() # many to many
+
+	def __str__(self):
+		return self.name
+	
 
 class PublisherLocationRelation(models.Model):
 	publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
@@ -128,6 +155,9 @@ class Publication(models.Model):
 	date = models.DateField()
 	location = models.ForeignKey(Location, on_delete=models.CASCADE)
 	e_text = models.FileField(upload_to='publication/') # ?
+
+	def __str__(self):
+		return 'work_name' # self.work.name
 
 class WorkPublicationRelation(models.Model): #many to many
 	work = '' # FK text | FK illustration
