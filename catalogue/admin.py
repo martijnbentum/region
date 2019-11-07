bfb31e5bead1cc3f5152e02bb6a1de02ec3d3c70
Martijn Bentum <bentummartijn@gmail.com>
2019-11-07 14:51:28 +0100

    registered models to admin page

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+import inspect
+import catalogue.models as models
+
+object_list = 'Audience,Book,Fragment,Genre,Illustration,Location,Periodical'
+object_list += ',Person,Publication,Publisher,PublisherManager,Text'
+object_list += ',PersonWorkRelation,PersonWorkRelationRole'
+object_list = object_list.split(',')
+
+
+for name, obj in inspect.getmembers(models):
+	if inspect.isclass(obj) and name in object_list:
+		admin.site.register(obj)
+
+
 
 # Register your models here.
