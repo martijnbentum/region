64bab50fc1f7613117b7af6ab44f1c1121ca5d59
Martijn Bentum <bentummartijn@gmail.com>
2019-11-13 16:05:35 +0100

    added comments added method to stringify person information

--- catalogue/models.py
+++ catalogue/models.py
@@ -13,6 +13,7 @@ class LocationType(models.Model, info):
 		return self.name
 
 class Location(models.Model, info):
+	'''Geographic location of a specific type (e.g. city or country)'''
 	name = models.CharField(max_length=200, default = '')
 	STATUS = [('F','fiction'), ('NF','non-fiction')]
 	status = models.CharField(max_length=2,choices=STATUS,default = 'NF')
@@ -27,6 +28,7 @@ class Location(models.Model, info):
 		return self.name 
 
 class LocationLocationRelation(models.Model, info):
+	'''defines a hierarchy of locations, e.g. a city is in a province.'''
 	container = models.ForeignKey('Location', related_name='container',
 									on_delete=models.CASCADE, default=None)
 	contained = models.ForeignKey('Location', related_name='contained',
@@ -38,6 +40,7 @@ class LocationLocationRelation(models.Model, info):
 
 
 class Person(models.Model, info):
+	'''A person with a specific role e.g. author, writer, etc.'''
 	first_name = models.CharField(max_length=200)
 	last_name = models.CharField(max_length=200)
 	pseudonyms = models.CharField(max_length=200,blank=True,null=True) 
@@ -45,23 +48,45 @@ class Person(models.Model, info):
 	gender = models.CharField(max_length=1,choices=GENDER)
 	date_of_birth = models.DateField(blank=True,null=True)
 	date_of_death = models.DateField(blank=True,null=True)
-	residence = models.ForeignKey(Location, on_delete=models.CASCADE) # multiple residences?? duplicate of PersonLocationRelation??
+	residence = models.ForeignKey(Location, on_delete=models.CASCADE) 
+	# multiple residences?? duplicate of PersonLocationRelation??
+	# should be one to many??
 	notes = models.TextField(blank=True,null=True) # one to many
 	
 	def __str__(self):
 		return self.first_name + ' ' + self.last_name
 
+	@property
+	def attribute_names(self):
+		m = 'first_name,last_name,pseudonyms,gender,date_of_birth,date_of_death'
+		m += ',residence,notes'
+		return m.split(',')
+
+	@property
+	def listify(self):
+		m = []
+		for attr in self.attribute_names:
+			if attr == 'gender': m.append(dict(self.GENDER)[self.gender])
+			elif 'date_of' in attr: 
+				try:temp = getattr(self,attr).ctime().split(' ')
+				except: m.append('no date')
+				else:m.append(' '.join(temp[:3]) +' '+temp[4]) 
+			else: m.append(str(getattr(self,attr)))
+		return m
+
 class PersonLocationRelation(models.Model):
+	'''location function for a person e.g. residence, work, travel.'''
 	person = models.ForeignKey(Person, on_delete=models.CASCADE)
 	location = models.ForeignKey(Location, on_delete=models.CASCADE)
-	LOCATION_TYPE = [('R','residence'),('T','travel')]
-	location_type = models.CharField(max_length=1,choices=LOCATION_TYPE)
+	#hard coded location function good idea?
+	LOCATION_FUNCTION= [('R','residence'),('T','travel')]
+	location_function= models.CharField(max_length=1,choices= LOCATION_FUNCTION)
 	start_date = models.DateField()
 	end_date = models.DateField()
 
 
-
 class Genre(models.Model, info):
+	'''category for texts (and maybe illustrations?) needs a relation as well?L'''
 	name = models.CharField(max_length=100)
 	description = models.TextField(blank=True)
 	
@@ -69,6 +94,7 @@ class Genre(models.Model, info):
 		return self.name
 
 class Text(models.Model, info):
+	'''a text can be an entire book or article or a subsection thereof.'''
 	title = models.CharField(max_length=300)
 	language = models.CharField(max_length=100)
 	genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
@@ -81,6 +107,7 @@ class Text(models.Model, info):
 		return self.title
 
 class TextTextRelationType(models.Model, info):
+	'''category of text2text relations e.g. a translation, review etc.'''
 	name = models.CharField(max_length=100)
 	description = models.TextField(blank=True)
 
@@ -88,6 +115,8 @@ class TextTextRelationType(models.Model, info):
 		return self.name
 	
 class TextTextRelation(models.Model, info):
+	'''connects two texts with a specific type of relation e.g. original 
+	and translation (primary, secondary).'''
 	primary = models.ForeignKey('Text', related_name='primary',
 									on_delete=models.CASCADE, default=None)
 	secondary = models.ForeignKey('Text', related_name='secondary',
@@ -101,7 +130,7 @@ class TextTextRelation(models.Model, info):
 		return m
 
 class Fragment(models.Model):
-	'''What is the function of fragment'''
+	'''What is the function of fragment, could be a binary flag on text'''
 	#fk text
 	#contents
 	pass
@@ -109,6 +138,7 @@ class Fragment(models.Model):
 
 
 class Illustration(models.Model, info):
+	'''a picture, should the picture itself be saveable?, illustration format?'''
 	caption =  models.CharField(max_length=300,null=True,blank=True)
 	language = models.CharField(max_length=100,null=True,blank=True)
 	context = models.TextField(null=True,blank=True)
@@ -119,10 +149,12 @@ class Illustration(models.Model, info):
 		return self.caption
 
 class IllustrationCategory(models.Model):
+	'''could also be genre?'''
 	category = models.CharField(max_length=100)
 	description = models.TextField(null=True,blank=True)
 
 class IllustrationCategoryRelation(models.Model): # many to many
+	'''links a category to an illustration.'''
 	illustration = models.ForeignKey(Illustration, on_delete=models.CASCADE)
 	category = models.ForeignKey(IllustrationCategory, on_delete=models.CASCADE)
 
@@ -169,6 +201,7 @@ class PersonWorkRelation(models.Model, info):
 
 
 class Periodical(models.Model, info):
+	'''Recurrent publication.'''
 	title = models.CharField(max_length=300)
 	language = models.CharField(max_length=100)
 	start_date = models.DateField()
@@ -195,6 +228,7 @@ class Book(models.Model, info):
 
 
 class Publisher(models.Model, info):
+	'''Company that publishes works.'''
 	name = models.CharField(max_length=300)
 	location= models.ManyToManyField(Location,blank=True)
 	start_date = models.DateField(null=True,blank=True)
@@ -205,10 +239,12 @@ class Publisher(models.Model, info):
 		return self.name
 	
 class PublisherManager(models.Model): #or broker
+	'''Person that manages writers, should be linked to texts and creators?'''
 	publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
 	manager = models.ForeignKey(Person, on_delete=models.CASCADE)
 
 class Publication(models.Model, info):
+	'''A specific instance of a text and or illustration or periodical or book?'''
 	publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
 	form = '' # FK periodical | FK book
 	issue = models.PositiveIntegerField(null=True,blank=True) 
@@ -222,6 +258,7 @@ class Publication(models.Model, info):
 		return 'work_name' # self.work.name
 
 class WorkPublicationRelation(models.Model): #many to many
+	'''Links a work with a publication.'''
 	work = '' # FK text | FK illustration
 	publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
 
