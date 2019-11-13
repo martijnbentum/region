7766b002365e3f47626ae1cc24e9ec3ec8a513c4
Martijn Bentum <bentummartijn@gmail.com>
2019-11-13 16:08:16 +0100

    defined a person view

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Person
+
+class PersonView(generic.ListView):
+	template_name = 'catalogue/index.html'
+	context_object_name = 'person_list'
+
+	def get_queryset(self):
+		return Person.objects.order_by('last_name')
 
 # Create your views here.
