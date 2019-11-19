6ee6f5c2a4330214a04b9190f88713bf24ca828e
Martijn Bentum <bentummartijn@gmail.com>
2019-11-19 17:24:45 +0100

    added urls

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import include,path,re_path
 
 from . import views
 
 app_name = 'catalogue'
 urlpatterns = [
-	path('', views.PersonView.as_view(), name='person'),
+	path('', views.get_locationname, name='location_name'),
+	path('person/',views.PersonView.as_view(),name='person_view'),
+	path('location/',views.get_locationname, 'location_name'),
+	path('location_name/', views.get_locationname, name='location_name'),
+	path('add_person/', views.add_person, name='add_person'),
+	re_path(r'^select2/', include('django_select2.urls')),
+	# path('location_name', views.get_locationname, name='location_name'),
 ]
