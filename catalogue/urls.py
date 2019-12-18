f241091a64b50e5b4db7de67eaf1affedb3be444
Martijn Bentum <bentummartijn@gmail.com>
2019-12-18 14:50:56 +0100

    changed person paths

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -2,20 +2,16 @@ from django.urls import include,path,re_path
 
 from . import views
 
-aplr = views.add_person_location_relation
 
 app_name = 'catalogue'
 urlpatterns = [
 	path('', views.PersonView.as_view(), name='person_view'),
-	path('<int:person_id>/person/',views.add_person,name='person_detail'),
-	path('person/',views.PersonView.as_view(),name='person_view'),
-	path('location/',views.LocationView.as_view(),name='location_view'),
-	path('text/',views.TextView.as_view(),name='text_view'),
 	path('add_location/',views.add_location, name='add_location'),
-	path('add_person/', views.add_person, name='add_person'),
-	# path('add_person/', views.PersonCreate.as_view(), name='add_person'),
-	path('add_person_location/', aplr, name='add_person_location'),
+	path('location/',views.LocationView.as_view(),name='location_view'),
 	path('add_text/', views.add_text, name='add_text'),
+	path('text/',views.TextView.as_view(),name='text_view'),
+	path('add_person/', views.add_person, name='add_person'),
+	path('edit_person/<int:person_id>', views.edit_person, name='edit_person'),
+	path('person/<int:person_id>',views.edit_person,name='person_detail'),
 	re_path(r'^select2/', include('django_select2.urls')),
-	# path('location_name', views.get_locationname, name='location_name'),
 ]
