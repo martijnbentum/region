2a52d6c4e3a4ebc4f0dfc4434960a367d08f4001
Martijn Bentum <bentummartijn@gmail.com>
2020-01-10 09:48:08 +0100

    fixed bug with add_person / edit_person views

--- catalogue/views.py
+++ catalogue/views.py
@@ -5,7 +5,7 @@ from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse
 from .models import Date, Location, Person, Text,PersonLocationRelation
 from .forms import PersonForm, LocationForm, TextForm, PersonLocationRelationForm
-from .forms import location_formset, helper
+from .forms import location_formset 
 from django.forms import inlineformset_factory
 import json
 
@@ -66,11 +66,8 @@ def _handle_date(goal_object,form,names=None):
 		names = 'date,start_date,end_date,start_spec,end_spec'
 	date_name,start_date,end_date,start_spec,end_spec = names.split(',')
 	f = form
-	print(f,goal_object.pk)
-	print(names)
 	try: date = getattr(goal_object,date_name)
 	except: date = None
-	print(date)
 	if date == None:
 		d = Date(start=f[start_date],end=f[end_date],
 			start_specificity=f[start_spec],
@@ -86,76 +83,42 @@ def _handle_date(goal_object,form,names=None):
 	goal_object.save()
 	# print(goal_object.view())
 
+
 def _handle_plr_formset(formset):
-	# formset.save(commit=False)
+	'''handle inline formset for personlocation relation'''
 	plrs = formset.save(commit = False)
-	print(9,plrs[0])
 	for f,plr in zip(formset.cleaned_data,plrs):
-		print(f,f['DELETE'],111222)
-		if f['DELETE']:continue
+		if f['DELETE']: continue
 		if not f['location']: continue
-		'''
-		d = Date(start=f['start_date'],end=f['end_date'],
-			start_specificity=f['start_spec'],
-			end_specificity=f['end_spec'])
-		d.save()
-		print(d.view())
-		# print(plr.date.view(),'---')
-		plr.date = d
-		# print(plr.date.view(),'-0-')
-		plr.save()
-		'''
-		print(plr,8)
 		_handle_date(plr,f)
-	print('--')
 	[obj.delete() for obj in formset.deleted_objects]
 
+
 def _handle_person_form(form):
+	'''create or edit person instance 
+	date is an object and needs to be created seperately 
+	handle date object creation and saving
+	'''
 	f = form.cleaned_data
-	'''
-	print(f,f['date_of_death'],type(f['date_of_death']))
-	d = Date(start=f['date_of_birth'],end=f['date_of_death'],
-		start_specificity=f['birth_spec'],end_specificity=f['death_spec'])
-	print(d.view())
-	d.save()
-	person= form.save()
-	print(person.view())
-	# print(person.birth_death_date.view(),9)
-	person.birth_death_date = d
-	# print(person.birth_death_date.view(),0)
-	person.save()
-	'''
 	person= form.save()
 	names = 'birth_death_date,date_of_birth,date_of_death,birth_spec,death_spec'
 	_handle_date(person,f,names=names)
 	return person
 	
 
-
 def add_person(request):
-	# if this is a post request we need to process the form data
+	'''create a person instance from person form
+	a date and a personlocationrelation object might need to be created
+	this leads to more involved form handling
+	'''
 	if request.method == 'POST':
 		form = PersonForm(request.POST)
 		if form.is_valid():
 			person = _handle_person_form(form)
-			formset = location_formset(request.POST, request.FILES, instance=person)
-			print(formset,'--------',formset.is_valid())
+			formset = location_formset(request.POST, 
+				request.FILES, instance=person)
 			if formset.is_valid():
 				_handle_plr_formset(formset)
-				'''
-				plrs = formset.save(commit = False)
-				for f,plr in zip(formset.cleaned_data,plrs):
-					print(f)
-					d = Date(start=f['start_date'],end=f['end_date'],
-						start_specificity=f['start_spec'],
-						end_specificity=f['end_spec'])
-					d.save()
-					print(d.view())
-					plr.date = d
-					plr.save()
-				print('--')
-				# plr.date = d
-				'''
 				return HttpResponseRedirect(reverse('catalogue:edit_person', 
 					args=[person.pk]))
 	form = PersonForm()
@@ -164,23 +127,12 @@ def add_person(request):
 	var = {'form':form,'formset':formset,'page_name':page_name}
 	return render(request, 'catalogue/add_person.html', var)
 
-
-def edit_person(request, person_id):
-	person = Person.objects.get(pk=person_id)
-	if request.method == 'POST':
-		formset = location_formset(request.POST, request.FILES, instance=person)
-		print(formset,'--------',formset.is_valid())
-		form = PersonForm(request.POST,instance=person)
-		if form.is_valid() and formset.is_valid():
-			person = _handle_person_form(form)
-			_handle_plr_formset(formset)
-			return HttpResponseRedirect(reverse('catalogue:edit_person', 
-				args = [person.pk]))
+def _populate_personform(person):
+	'''populate personform with pre existing data.'''
 	form = PersonForm(instance=person)
 	sd, ed = form.fields['date_of_birth'], form.fields['date_of_death']
 	ss, es = form.fields['birth_spec'], form.fields['death_spec']
 	bdd = form.instance.birth_death_date
-	print(bdd)
 	if bdd: 
 		sd.widget.attrs['value'] = sd.clean(bdd.start)
 		ed.widget.attrs['value'] = ed.clean(bdd.end)
@@ -200,10 +152,24 @@ def edit_person(request, person_id):
 			ies = f.instance.date.end_specificity
 			ss.initial = ss.clean(iss)
 			es.initial = es.clean(ies)
+	return form, formset
+
+def edit_person(request, person_id):
+	'''edit person instance
+	form does not follow person model therefore more involved form and formset
+	handling is needed
+	'''
+	person = Person.objects.get(pk=person_id)
+	if request.method == 'POST':
+		formset = location_formset(request.POST, request.FILES, instance=person)
+		form = PersonForm(request.POST,instance=person)
+		if form.is_valid() and formset.is_valid():
+			person = _handle_person_form(form)
+			_handle_plr_formset(formset)
+			return HttpResponseRedirect(reverse('catalogue:edit_person', 
+				args = [person.pk]))
+	form, formset = _populate_personform(person)
 		
-			print(sd.initial, ed.initial,ss.initial,es.initial)
-	# print(form.instance.birth_death_date)
-	# print(form.instance,formset.instance)
 	page_name = 'Edit Person'
 	var = {'form':form,'formset':formset,'page_name':page_name}
 	return render(request, 'catalogue/add_person.html',var)
@@ -213,10 +179,7 @@ def add_person_location_relation(request, person_id=None):
 	# if this is a post request we need to process the form data
 	if request.method == 'POST':
 		form = PersonForm(request.POST)
-		# print(form,form.is_valid()
 		if form.is_valid():
-			print('form is valid: ',form.cleaned_data,type(form))
-			# form.instance.residence = form.cleaned_data["residence"]#[0]
 			form.save()
 			return HttpResponseRedirect('/person/')
 	else:
