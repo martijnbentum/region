4d76b2a874f155854b3a4d112ac5307eb54a642e
Martijn Bentum <bentummartijn@gmail.com>
2020-02-04 09:40:43 +0100

    language widget

--- utilities/forms.py
+++ utilities/forms.py
@@ -2,6 +2,20 @@ from django import forms
 from django.forms import ModelForm, inlineformset_factory
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Fieldset
+from django_select2.forms import ModelSelect2Widget
+from .models import Date, Language
+
+
+class LanguageWidget(ModelSelect2Widget):
+	model = Language
+	search_fields = ['name__startswith']
+
+	def label_from_instance(self,obj):
+		return obj.name
+
+	def get_queryset(self):
+		return Language.objects.all().order_by('name')
+
 
 class DateForm(ModelForm):
 	'''form to add a person'''
