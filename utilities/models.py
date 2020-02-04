ac7d5ea3a35076e2a7692e3b45e3a72fcfef3276
Martijn Bentum <bentummartijn@gmail.com>
2020-02-04 09:41:11 +0100

    made language name unique

--- utilities/models.py
+++ utilities/models.py
@@ -85,7 +85,7 @@ class Date(models.Model, info):
 
 
 class Language(models.Model, info):
-	name = models.CharField(max_length=100)
+	name = models.CharField(max_length=100, unique = True)
 	iso = models.CharField(max_length=3,null=True,blank=True)
 
 	def __str__(self):
