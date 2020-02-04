4cda2a3d17ba0cca1bf3aea8903c6b929ad05675
Martijn Bentum <bentummartijn@gmail.com>
2020-02-04 09:39:05 +0100

    widget for multiple select2

--- locations/forms.py
+++ locations/forms.py
@@ -1,7 +1,18 @@
-from django_select2.forms import ModelSelect2Widget
+from django_select2.forms import ModelSelect2Widget, ModelSelect2MultipleWidget
 from .models import UserLoc
 
 class LocationWidget(ModelSelect2Widget):
+	model = UserLoc 
+	search_fields = ['name__icontains']
+
+	def label_from_instance(self,obj):
+		return obj.name
+
+	def get_queryset(self):
+		return UserLoc.objects.all().order_by('name')
+
+
+class LocationsWidget(ModelSelect2MultipleWidget):
 	model = UserLoc 
 	search_fields = ['name__icontains']
 
