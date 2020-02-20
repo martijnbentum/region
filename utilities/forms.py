c7c1b5e33af66d8a0c9cf14d89981ebfd6c2fbc6
Martijn Bentum <bentummartijn@gmail.com>
2020-02-20 09:59:03 +0100

    removed date object

--- utilities/forms.py
+++ utilities/forms.py
@@ -3,7 +3,7 @@ from django.forms import ModelForm, inlineformset_factory
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Fieldset
 from django_select2.forms import ModelSelect2Widget
-from .models import Date, Language
+from .models import Language
 
 
 class LanguageWidget(ModelSelect2Widget):
@@ -17,40 +17,3 @@ class LanguageWidget(ModelSelect2Widget):
 		return Language.objects.all().order_by('name')
 
 
-class DateForm(ModelForm):
-	'''form to add a person'''
-
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-		self.helper = formhelper()
-		self.helper.add_input(
-			submit('submit', 'save', css_class='btn-success'))
-		self.helper.form_method = 'post'
-
-		self.helper.layout= layout(
-			row(
-				column('start', css_class='form-group col-md-6 mb-0'),
-				column('end', css_class='form-group col-md-6 mb-0'),
-				css_class='from-row'
-			),
-			'start_specificity',
-			'end_specificity',
-			)
-
-	class Meta:
-		model = Date
-		fields = 'start,end,start_specificity,end_specificity'.split(',')
-		attrs={'class':'form-control',
-			'type':'date'}
-		widgets = {
-			'date_start': forms.DateInput(
-				format=('%d %m $Y'),
-				attrs= attrs
-				),
-			'date_end': forms.DateInput(
-				format=('%d %m $Y'),
-				attrs= attrs
-				)
-		}
-		labels = {'start_spec':'start specificity',
-			'end_spec':'end specificity'}
