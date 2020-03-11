d630be98053b3a785363c2d6c795bf674f81bab9
Martijn Bentum <bentummartijn@gmail.com>
2020-03-11 10:42:22 +0100

    moved crud class to utils/view_util.py added getfocus function to get the in focus tabs

--- utilities/views.py
+++ utilities/views.py
@@ -2,164 +2,6 @@ from django.apps import apps
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from utils import view_util
-from easyaudit.models import CRUDEvent
-
-class Cruds:
-	def __init__(self,app_name,model_name):
-		self.app_name = app_name
-		self.model_name = model_name
-		self.model = apps.get_model(app_name,model_name)
-		self.instances = self.model.objects.all()
-		self.cruds = [Crud(i) for i in self.instances]
-		self.cruds.sort(reverse = True)
-
-	def __repr__(self):
-		return ' '.join([self.app_name,self.model_name,'n:',str(self.ninstances)])
-
-	@property
-	def ninstances(self):
-		return len(self.cruds)
-
-	@property
-	def last_update(self):
-		return self.cruds[0].last_update
-		
-
-class Crud:
-	def __init__(self,instance):
-		i = instance
-		self.instance = instance
-		self.model_name = str(type(i)).split('.')[-1].split("'")[0].lower()
-		self.app_name = str(type(i)).split('.')[0].split("'")[-1].lower()
-		self.get_crud_events()
-
-	def get_crud_events(self):
-		events= CRUDEvent.objects.filter(
-			content_type__model=self.model_name,object_id=self.instance.pk)
-		self.events = [Event(e) for e in events]
-
-	def __lt__(self,other):
-		if len(self.events) == 0: return True
-		if len(other.events) == 0: return False
-		return self.events[0].epoch < other.events[0].epoch
-
-	def __repr__(self):
-		return self.__str__()
-
-	def __str__(self):
-		return ' // '.join([self.app_name,self.model_name, self.last_update])
-
-	@property
-	def contributers(self):
-		return ' | '.join([e.user.username for e in self.events])
-
-	@property
-	def created(self):
-		for e in self.events:
-			if e.type == 'Create':
-				return  e.username + ' ' + e.time_str
-		return 'user unknown'
-
-	def _make_change_fields_string(self, e):# changed_fields):
-		if not e.changed: return ['no changes']
-		o = []
-		for k in e.cf_dict:
-			old, new = e.cf_dict[k]
-			o.append( k + ': ' + old + ' -> ' + new )
-		return o
-
-
-	@property
-	def updates(self):
-		return [e for e in self.events if e.type =='Update']
-
-
-	@property
-	def anychanges(self):
-		return True if self.updates else False
-		
-
-	@property
-	def updates_str(self):
-		o = []
-		for e in self.updates:
-			cfs = self._make_change_fields_string(e)
-			for cf in cfs:
-				o.append(' | '.join([e.username,
-					e.time_str,cf]))
-		if o == []: return 'no updates'
-		return o
-
-	@property
-	def last_update(self):
-		if len(self.events) == 0: return 'unknown'
-		e = self.events[0]
-		if len(self.events) ==1 and e.type == 'Create':
-			cf = 'created'
-		else:
-			cf = ' | '.join(self._make_change_fields_string(e))
-		return ' // '.join([e.time_str,e.username,cf])
-
-	@property
-	def last_update_time(self):
-		if len(self.events) == 0: return 'time unknown'
-		return self.events[0].time_str_exact
-	
-	@property
-	def last_update_by(self):
-		if len(self.events) == 0: return 'user unknown'
-		return self.events[0].username
-
-class Event:
-	def __init__(self,e):
-		self.event = e
-		self.type = e.get_event_type_display()
-		self.changed = True if e.changed_fields not in ['null',None] else False
-		self.username = e.user.username
-		self.set_time()
-		if self.changed: self.set_changes()
-
-	def set_changes(self):
-		try: self.cf_dict = eval(self.event.changed_fields)
-		except: raise ValueError('could not create dict from:',
-			self.event.changed_fields)
-		self.changes = [Change(self.username,self.time_str,k,self.cf_dict[k])
-			for k in self.cf_dict.keys()]
-
-	def set_time(self):
-		e = self.event
-		dt = e.datetime
-		self.time_str_exact= dt.strftime('%d %B %Y %H:%M')
-		self.time_str_day_short= dt.strftime('%d %B %Y')
-		self.time_str_hour_short= dt.strftime('%H:%M')
-		self.epoch = dt.timestamp()
-		self.time_delta = dt.now().timestamp() - self.epoch
-		temp = dt.now().strftime('%d %B %Y') == dt.strftime('%d %B %Y') 
-		self.changed_today = temp
-		temp = dt.now().strftime('%Y') == dt.strftime('%Y') 
-		self.changed_thisyear = temp
-		self.days_ago = round(self.time_delta / (24*3600))
-		self.hours_ago = round(self.time_delta / 3600)
-		self.minutes_ago = round(self.time_delta / 60)
-		self.changed_recent= self.minutes_ago < 99
-		if self.minutes_ago < 3: ts = 'a moment ago'
-		elif self.changed_recent: ts = str(self.minutes_ago) + ' minutes ago'
-		elif self.hours_ago < 6: ts = str(self.hours_ago) + ' hours ago'
-		elif self.changed_today:ts = dt.strftime('today %H:%M') 
-		elif self.days_ago < 2: ts = dt.strftime('yesterday %H:%M')
-		elif self.days_ago < 6: ts = dt.strftime('last %A %H:%M')
-		elif self.changed_thisyear: ts = dt.strftime('%d %B')
-		else: ts = self.time_str_day_short
-		self.time_str = ts
-
-class Change:
-	def __init__(self,user,time,field_name,state):
-		self.username = user
-		self.time = time
-		self.field= field_name
-		self.old_state = state[0]
-		self.new_state = state[1]
-		
 		
 
 def add_simple_model(request, name_space,model_name,app_name, page_name):
@@ -175,4 +17,28 @@ def add_simple_model(request, name_space,model_name,app_name, page_name):
 	return render(request, 'persons/add_simple_model.html',var)
 
 
+def getnavs(request):
+	'''navs are variables to set the active tabs on a page.
+	navbar is the tab link
+	navcontent is the content link
+	'''
+	navbar, navcontent = 'default', None
+	if 'navbar' in request.POST.keys():
+		navbar = request.POST['navbar']
+	if 'navcontent' in request.POST.keys():
+		navcontent= request.POST['navcontent']
+	return navbar,navcontent
+
+
+def listify_navs(navbar,navcontent):
+	'''nav variables are csv in the url, need to transform them in a list.'''
+	if ',' in navbar: navbar = navbar.split(',')
+	if navcontent !=None: navcontent = navcontent.split(',')
+	return navbar,navcontent
+
+
+def getfocus(request):
+	if 'focus' in request.POST.keys():
+		return request.POST['focus']
+	else: return ''
 # Create your views here.
