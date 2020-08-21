abe94bb0e4f4c7e5789c1483866b7e05b71f3840
Martijn Bentum <bentummartijn@gmail.com>
2020-08-21 11:59:31 +0200

    instance2userloc_information now also catches m2m relations

--- utils/transition_location.py
+++ utils/transition_location.py
@@ -26,22 +26,37 @@ def geoloc2location(gl, save = False):
 	if save: l.save()
 	return l
 
+
+def _handle_ul(ul,fd,field_name):
+	location_type = ul.loc_type.name
+	location_precision = ul.loc_precision
+	location_status = ul.status
+	geonameids = '|'.join([gl.geonameid for gl in ul.geoloc_set.all()])
+	n = str(len(geonameids.split('|')))
+	information = [location_type,location_precision,location_status,geonameids,n]
+	if field_name not in fd.keys(): fd[field_name] = []
+	fd[field_name].append(information)
+	return fd
+
+def _get_fields(instance):
+	m2m = [[field,'m2m'] for field in instance._meta.__dict__['local_many_to_many']]
+	fk = [[field,'fk'] for field in instance._meta.fields]
+	return fk + m2m
+
 def instance2userloc_information(instance):
 	app_name, model_name = instance2names(instance)
+	fields = _get_fields(instance)
 	fd = {}
-	for field in instance._meta.fields:
+	for field,field_type in fields:
+		print(field.name,field_type)
 		try:related_model= str(field.remote_field.model).split(".")[-1].split("'")[0]
 		except:related_model = False
-		if related_model == 'UserLoc':  
-			ul = getattr(instance,field.name) 
-			if ul:
-				location_type = ul.loc_type.name
-				location_precision = ul.loc_precision
-				location_status = ul.status
-				geonameids = '|'.join([gl.geonameid for gl in ul.geoloc_set.all()])
-				n = str(len(geonameids.split('|')))
-				information = [location_type,location_precision,location_status,geonameids,n]
-				fd[field.name]= information
+		if related_model != 'UserLoc': continue
+		ul = getattr(instance,field.name) 
+		if field_type == 'fk' and ul: fd = _handle_ul(ul,fd,field.name)
+		elif field_type == 'm2m' and ul:
+			for x in ul.all():
+				fd = _handle_ul(x,fd,field.name)
 	return fd
 
 
