3111b44e3cfb71fcf2a21aef2ffa262336a8261e
Martijn Bentum <bentummartijn@gmail.com>
2020-09-24 14:49:52 +0200

    deleted whitespace

--- utilities/forms.py
+++ utilities/forms.py
@@ -20,3 +20,7 @@ def make_select2_attr(field_name = 'name', input_length = 2):
 	attr= {'attrs':{'data-placeholder':'Select by '+field_name+' ...',
 	'style':'width:100%','class':'searching','data-minimum-input-length':str(input_length)}} 
 	return attr
+
+
+
+
