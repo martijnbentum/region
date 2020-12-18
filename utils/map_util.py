e71763456fd2cbb7258057d0f8c784e21b16139a
Martijn Bentum <bentummartijn@gmail.com>
2020-12-18 16:01:39 +0100

    added a gps / latlng converter (str -> float tuple), moved some map related function to this module (from utilities.models) and some minor update to pop_up, last version before removing old style map vizualization from module

--- utils/map_util.py
+++ utils/map_util.py
@@ -1,7 +1,5 @@
-from utilities.models import instance2name, instance2names
-from locations.models import Location
+from .model_util import instance2name, instance2names
 import random
-from utilities.models import instance2name, instance2color, instance2icon, instance2map_buttons
 
 def instance2related_locations(instance):
 	'''returns all locations from related instances (foreign keys and m2m) 
@@ -42,7 +40,7 @@ def instance2related_locations(instance):
 					map_list.extend(instance2maprows(rl,role=fn))
 		if field_type == 'relation':
 			for rl in related_instance.all():
-				name = rl.other(source_model_name)
+				name = rl.other_field_name(source_model_name)
 				if name:
 					x = getattr(rl,name)
 					if not hasattr(x,'location_field'): continue
@@ -75,6 +73,7 @@ def field2locations(instance, field_name):
 	'''return locations from a field (fk or m2m) on a model.'''
 	if not hasattr(instance,field_name):return None
 	x = getattr(instance,field_name)
+	location_model = apps.get_model('locations','Location')
 	if type(x) == Location: return [x]
 	if 'ManyRelatedManager' in str(type(x)): 
 		return x.all()
@@ -163,7 +162,7 @@ def get_location_name(instance,latlng):
 	
 def pop_up(instance,latlng=None):
 	# location_name = get_location_name(instance,latlng)
-	location_name = instance.gps2name(latlng)
+	location_name = instance.latlng2name(latlng)
 	app_name, model_name = instance2names(instance)
 	m = ''
 	if hasattr(instance,'thumbnail'):
@@ -185,11 +184,72 @@ def pop_up(instance,latlng=None):
 	m += instance2map_buttons(instance)
 	if location_name:m += '<p><small>'+location_name+'</small></p>'
 	return m
+
+def gps2latlng(gps,return_one = False):
+	'''converts string with gps coordinates to tuple(s) of floats
+	gps 		string with format '43.2, 4.2' or '5.3, 4.5 | 6.4, 3.1'
+	output 		tuple/list of tuples of floats (43.2, 4.2) or [(5.3, 4.5), (6.4, 3.1)] 
+	'''
+	try:
+		o = [eval(x) for x in gps.split(' | ')]
+		if len(o) == 1 or return_one: o = o[0]
+	except: 
+		print('could not convert gps > latlng, input:',gps)
+		o = None
+	return o
 	
+def latlng2gps(latlng):
+	'''converts tuple or list of tuples with gps coordinates to string
+	latlng 		tuple/list of tuples of floats (43.2, 4.2) or [(5.3, 4.5), (6.4, 3.1)] 
+	output 		string with format '43.2, 4.2' or '5.3, 4.5 | 6.4, 3.1'
+	'''
+	if type(latlng) == tuple and len(latlng) == 2:
+		return str(latlng)
+	if type(latlng) == list:
+		error = False
+		for x in latlng:
+			if type(x) != tuple or len(x) != 2:error = True
+		if not error: return ' | '.join(map(str,latlng))
+	print('could not convert latlng > gps, input:',latlng)
+	return ''	
 
 def cull_maplist(maplist):
 	o = []
 	for line in maplist:
 		if line not in o: o.append(line)
 	return o
+
+
+def instance2color(instance):
+	name = instance2name(instance).lower()
+	if name in color_dict.keys(): return color_dict[name]
+	else: return 'black'
+
+def instance2icon(instance):
+	name = instance2name(instance).lower()
+	if name in icon_dict.keys():
+		return icon_dict[name]
+	return 'not found'
+
+def instance2map_buttons(instance):
+	app_name,model_name= instance2names(instance)
+	m = ''
+	m += '<a class = "btn btn-link btn-sm mt-1 pl-0 text-dark" href='
+	m += '/'+app_name+'/edit_' + model_name.lower()+'/' + str(instance.pk) 
+	m += ' role="button"><i class="far fa-edit"></i></a>'
+	m += '<a class = "btn btn-link btn-sm mt-1 pl-0 text-dark"'# href='
+	# m += '/locations/show_links/'+app_name+'/'+ model_name.lower()+'/' + str(instance.pk) +'/'
+	m += ' onclick="getLinks()"'
+	m += ' role="button"><i class="fas fa-project-diagram"></i></a>'
+	return m
+
+names = 'text,illustration,publisher,publication,periodical,person,movement'.split(',')
+colors = '#0fba62,#5aa5c4,#e04eed,#ed4c72,#1e662a,#c92f04,#e39817'.split(',')
+icons ='fa fa-file-text-o,fa fa-picture-o,fa fa-building-o,fa fa-book'
+icons +=',fa fa-newspaper-o,fa fa-male,fa fa-users'
+icons = ['<i class="'+icon+' fa-lg mt-2" aria-hidden="true"></i>' for icon in icons.split(',')]
+color_dict,icon_dict ={}, {}
+for i,name in enumerate(names):
+	color_dict[name] = colors[i]
+	icon_dict[name] = icons[i]
 		
