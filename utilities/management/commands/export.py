9000d236bb6cdb92e4faa0b00eabd24af207ef71
Martijn Bentum <bentummartijn@gmail.com>
2021-01-06 16:42:49 +0100

    command to export the databse to xlsx, xml or json with the utils/export module

---
+++ utilities/management/commands/export.py
@@ -0,0 +1,47 @@
+from django.core.management.base import BaseCommand
+from utils.export import Exports
+import time
+import os
+
+class Command(BaseCommand):
+	def add_arguments(self,parser):
+		parser.add_argument('-output',nargs='+')
+		parser.add_argument('-path')
+		parser.add_argument('--dryrun',action='store_true')
+
+
+	def handle(self, *args, **options):
+		start = time.time()
+		save = True if not options['dryrun'] else False
+		path = options['path']  
+		path = path if os.path.isdir(path) else ''
+		if path != '' and not path.endswith('/'): path += '/'
+
+		filename = path + 'region_'+time.strftime('%Y_%m_%d_%H_%M')
+		print('exporting the database...')
+		if options['output']:print('filetypes: ',', '.join(options['output']))
+		if save:
+			export = Exports()
+			export.make_exports()
+		if 'xlsx' in options['output']:
+			print('saving excel file:',filename)
+			if save: export.to_excel(filename = filename + '.xlsx')
+		if 'xml' in options['output']:
+			print('saving xml file:',filename)
+			if save:
+				with open(filename + '.xml', 'w') as fout:
+					fout.write(export.xml_str)
+		if 'json' in options['output']:
+			print('saving json file:',filename)
+			if save:
+				with open(filename + '.json', 'w') as fout:
+					fout.write(export.json_str)
+		print('done')
+		if not save: print('no files saved, this was a dryrun')
+		delta = time.time() -start
+		print('exporting took: ',round(delta),' seconds')
+		
+		
+
+
+
