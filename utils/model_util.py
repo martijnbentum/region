a3849dcb600b90cdac7f2dd3ab6aa49da68cd469
Martijn Bentum <bentummartijn@gmail.com>
2021-01-06 10:03:18 +0100

    copy models is moved from utilities/models

--- utils/model_util.py
+++ utils/model_util.py
@@ -91,6 +91,50 @@ def compare_queryset(qs):
 		complete_list.append(line)
 	return equal_list, similar_list, complete_list
 		
+
+def instance2names(instance):
+	# s = str(type(instance)).split("'")[-2]
+	# app_name,_,model_name = s.split('.')
+	app_name,model_name = instance._meta.app_label, instance._meta.model_name.capitalize()
+	return app_name, model_name
+
+def instance2name(instance):
+	app_name, model_name = instance2names(instance)
+	return model_name
 	
 		
+def copy_complete(instance, commit = True):
+	'''copy a model instance completely with all relations.'''
+	copy = simple_copy(instance, commit)
+	app_name, model_name = instance2names(instance)
+	for f in copy._meta.get_fields():
+		if f.one_to_many:
+			for r in list(getattr(instance,f.name+'_set').all()):
+				rcopy = simple_copy(r,False,False)
+				setattr(rcopy,model_name.lower(), copy)
+				rcopy.save()
+		if f.many_to_many:
+			getattr(copy,f.name).set(getattr(instance,f.name).all())
+	return copy
+
+
+def simple_copy(instance, commit = True,add_copy_suffix = True):
+	'''Copy a model instance and save it to the database.
+	m2m and relations are not saved.
+	'''
+	app_name, model_name = instance2names(instance)
+	model = apps.get_model(app_name,model_name)
+	copy = model.objects.get(pk=instance.pk)
+	copy.pk = None
+	print('copying...')
+	for name in 'title,name,caption,first_name'.split(','):
+		if hasattr(copy,name):
+			print('setting',name)
+			copy.view()
+			setattr(copy,name,getattr(copy,name)+ ' !copy!')
+			copy.view()
+			break
+	if commit:
+		copy.save()
+	return copy
 		
