12455e3c9fe806bd5285736050ba572322daf52c
Martijn Bentum <bentummartijn@gmail.com>
2021-05-07 09:31:16 +0200

    added timeline url

--- utilities/urls.py
+++ utilities/urls.py
@@ -7,5 +7,7 @@ app_name = 'utilities'
 urlpatterns = [
 	path('close/',views.close,name='close'),
 	path('list_view/<str:model_name>/<str:app_name>/',views.list_view,name='list_view'),
-	path('list_view/<str:model_name>/<str:app_name>/<int:max_entries>',views.list_view,name='list_view'),
+	path('list_view/<str:model_name>/<str:app_name>/<int:max_entries>',views.list_view,
+		name='list_view'),
+	path('timeline/',views.timeline,name='timeline'),
 ]
