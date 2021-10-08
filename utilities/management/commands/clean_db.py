e4ab2b83156f15be03a5fd7aa4b28495655414e2
Martijn Bentum <bentummartijn@gmail.com>
2021-10-08 14:52:30 +0200

    layout change;typo fix

--- utilities/management/commands/clean_db.py
+++ utilities/management/commands/clean_db.py
@@ -7,10 +7,11 @@ import os
 
 '''
 CRUDEvent creates an db instance for each change to the database.
-This can blow up the size of the database, especially with programatic changes to instances.
+This can blow up the size of the database, especially with 
+programatic changes to instances.
 
 CRUD events without changes (saving an instance without changing anything
-or wihtout a user (programmatic changes) are delete to save disk space
+or wihtout a user (programmatic changes) are deleted to save disk space
 '''
 
 
@@ -18,7 +19,8 @@ class Command(BaseCommand):
 
 	def handle(self, *args, **options):
 		start = time.time()
-		print('cleaning database by removing superious CRUD events and VACUUM the database')
+		print('cleaning database by removing superious CRUD events')
+		print('and VACUUM the database')
 		dbsize_before = get_db_size()
 		print('database size:',dbsize_before,'MB')
 		c = CRUDEvent.objects.all()
@@ -27,7 +29,8 @@ class Command(BaseCommand):
 		o = []
 		for event in c:
 			if not event.user or event.changed_fields == 'null': o.append(event)
-		print('removing',len(o),'spurious CRUD events (without user or changed fields)')
+		print('removing',len(o),'spurious CRUD events') 
+		print('(without user or changed fields)')
 		for event in o:
 			event.delete()
 
