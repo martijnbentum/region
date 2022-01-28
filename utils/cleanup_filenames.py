285d74765c931dcc052fc15cc36903147f93ccb1
Martijn Bentum <bentummartijn@gmail.com>
2022-01-28 14:58:20 +0100

    helper function to change filenames of uploaded files; function to remove diacritics from filename, linux does not handle diacritics well

---
+++ utils/cleanup_filenames.py
@@ -0,0 +1,27 @@
+import os
+import dcl
+
+def rename_file(instance,file_fieldname,new_name, overwrite = False):
+	file_field = getattr(instance,file_fieldname)
+	old_path = file_field.path
+	base_dir = old_path.replace(file_field.name,'')
+	new_path = base_dir + new_name
+	if not overwrite: assert not os.path.isfile(new_path)
+	os.rename(old_path,new_path)
+	setattr(instance,file_fieldname,new_name)
+	instance.save()
+
+def remove_diacritics_filename(instance,file_fieldname):
+	file_field = getattr(instance,file_fieldname)
+	if not dcl.has_diacritics(file_field.name):return
+	new_name = dcl.clean_diacritics(file_field.name)
+	rename_file(instance,file_fieldname,new_name)
+	print(new_name[-10:],'updated filename')
+
+def rename_all_dicritic_illustration_filenames():
+	from catalogue.models import Illustration
+	i = Illustration.objects.all()
+	for x in i:
+		if not x.upload: continue
+		remove_diacritics_filename(x,'upload')
+
