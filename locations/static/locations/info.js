46f2edd2e889b9f76b336c0e6c1df967f22d2091
Martijn Bentum <bentummartijn@gmail.com>
2023-06-02 14:49:03 +0200

    added show_info as show_top_bar_info to info.js module

--- locations/static/locations/info.js
+++ locations/static/locations/info.js
@@ -50,4 +50,39 @@ class Info {
 
 }
 
-export {Info}
+function show_top_bar_info(index) {
+	// shows general information (number of linked instances) 
+	// about a location at the top of the screen
+    console.log(index);
+	var label = document.getElementById('city_label');
+	if (info.markers.clustered_marker_indices.includes(index)) {
+		//multiple locations are clustered into 1 marker
+		var elements = info.markers.clustered_marker_dict[index].elements; 
+        var html = '';
+		for (let i=0; i < elements.length; i++) {
+			var el= elements[i];
+			var information = info.markers.d[el.options.index];
+			html += information.name
+			html += '<small> (' + information.count + ')</small>';
+			if (html.length > 140) {
+				html += '<small> [...] + ' + (elements.length - 1 - i) 
+                html += ' locations'
+				break
+			}
+			if (i != elements.length -1) { html += ', '; }
+		}
+	} else {
+        if (info.connection_view) 
+            { var information = info.markers.connection_d[index]; }
+		else { var information =info.markers.d[index]; }
+		var html = information.name;
+		html += '<small> (' + information.count + ' entries) ';
+		for (const x of information.model_names) {
+			if (x) { html += x.toLowerCase() + ' '}
+		}
+		html += '</small>'
+	}
+	label.innerHTML = html
+}
+
+export {Info, show_top_bar_info}
