fd064ca021e21076b397ceffdf33d86fb80f8b8f
Martijn Bentum <bentummartijn@gmail.com>
2023-06-03 09:36:11 +0200

    refactor to filter module

--- locations/static/locations/map_rf.js
+++ locations/static/locations/map_rf.js
@@ -12,20 +12,14 @@ import {
 } from './marker.js';
 
 import {Info,show_top_bar_info} from './info.js'
-import {
-    open_left_sidebar,
-    open_right_sidebar,
-    close_right_sidebar,
-    close_left_sidebar,
-    Sidebar,
-    } from './sidebar.js'
-    
+import {Sidebar} from './sidebar.js'
+
 var info = new Info();
 var sidebar = new Sidebar();
 
 function set_location_type() {
     //console.log(location_type,location_type.value);
-    update_right_sidebar();
+    sidebar.update_right_sidebar(info);
 }
 
 async function get_instance(instance_id,instance_category) {
@@ -113,8 +107,8 @@ async function get_connections(instance_identifier) {
     console.log('connection data', data, 
         data.instances, data.instances.length);
     info.markers.hide_markers(info.markers.layerDict['overview']);
-    close_left_sidebar();
-    clear_right_sidebar();
+    sidebar.close_left_sidebar();
+    sidebar.clear_right_sidebar(info);
     info.markers.connection_d = Object.values(data.instances);
     for (i = 0; i<info.markers.connection_d.length; i++) {
             console.log(info.markers.connection_d[i], i, 1111)
@@ -168,80 +162,6 @@ function count_active_identifiers(identifiers) {
     return count;
 }
 
-function show_category(instance_ids, category,city_div) {
-	// get category information and create an html element to 
-    //display it in the sidebar
-	// the category (e.g. Text) the corresponding instances are loaded via ajax
-	var model_name = category.split('_')[1]
-    if (info.filter_active_dict['model,'+model_name] == 'inactive') { return; }
-	var identifiers=pks_and_category_to_identifiers(instance_ids,category);
-    var count_active = count_active_identifiers(identifiers) 
-    if (count_active == 0) { return;}
-	// var sidebar= document.getElementById('sidebar-content');
-	var div = document.createElement('div')
-	info.entries.push(div);
-	div.id = model_name + '-all-' + city_div.id;
-	city_div.appendChild(div);
-    //console.log(instance_ids)
-	get_instances(instance_ids, category, city_div)
-	var a =document.createElement("a");
-    a.id = model_name + 'category-toggle-' + city_div.id
-	a.setAttribute('href',"javascript:void(0)");
-	a.setAttribute('onclick', 'toggle_sidebar_category(this)');
-	a.setAttribute('data-links_id', model_name + '-links-'+city_div.id);
-	var identifiers=pks_and_category_to_identifiers(instance_ids,category);
-	a.setAttribute('data-identifiers',identifiers);
-	div.appendChild(a);
-	a.innerHTML = model_name + ' <small>(' + count_active + ')</small>';
-	a.classList.add('category-header');
-    info.right_sidebar_category_counts[model_name] = count_active;
-}
-
-function show_categories(information) {
-	// show categories (e.g. Text, Person) in the sidebar 
-	// linked to a given location 
-	var sidebar= document.getElementById('right_sidebar_content');
-	var city_div =document.createElement("d");
-	sidebar.appendChild(city_div)
-	city_div.id = information.pk + "-links";
-	info.entries.push(city_div)
-	for (const [key, instance_ids] of Object.entries(information)) {
-		if ('count,model_names,name,gps,pk,identifiers'.split(',').includes(key)){
-		} else {
-			show_category(instance_ids, key, city_div);
-		}
-	}
-}
-
-function show_city(information) {
-	//show all information linked to specific location
-	var sidebar= document.getElementById('right_sidebar_content');
-	var div =document.createElement("d");
-	sidebar.appendChild(div);
-	info.entries.push(div)
-	var a =document.createElement("a");
-	a.setAttribute('href',"javascript:void(0)");
-	a.setAttribute('onclick', 'toggle_sidebar_category(this)');
-	a.setAttribute('data-links_id', information.pk + '-links');
-	a.setAttribute('data-color_inactive', 'grey');
-	a.setAttribute('data-color_active', '#2f3030');
-	a.classList.add("city-header");
-	div.appendChild(a);
-	// var title = document.getElementById('title');
-	var html = information.name;
-	html += '<small> (' + information.count + ' entries) ' + '</small>';
-	a.innerHTML = html
-	show_categories(information);
-}
-
-function clear_right_sidebar() {
-	//remove old entries from sidebar
-	var sidebar= document.getElementById('right_sidebar_content');
-	for (const x of info.entries) {
-		x.remove()
-	}
-	info.entries = [];
-}
 
 function _add_connection_instance_info(instance_dict, original) {
     //show an instance in the text connection view right side bar
@@ -301,12 +221,12 @@ function _add_other_connection_info(instance_dicts, other_name) {
 
 function back_to_overview() {
     console.log('back to overview');
-    clear_right_sidebar();
+    sidebar.clear_right_sidebar(info);
     info.markers.hide_markers(info.markers.layerDict['connection_view']);
     info.markers.layerDict['connection_view'] = [];
-    open_left_sidebar();
+    sidebar.open_left_sidebar();
     info.markers.update_markers(info.markers.active_markers);
-    show_right_sidebar(info.right_sidebar_index);    
+    sidebar.show_right_sidebar(info);    
     console.log('rsi',info.right_sidebar_index);
     info.connection_view = false;
 }
@@ -339,61 +259,6 @@ function right_sidebar_connection_view_text(data) {
     _add_other_connection_info(cd.reviews, 'Reviews')
 }
 
-function show_right_sidebar(index) {
-	//show sidebar with entries from selected location
-	clear_right_sidebar();
-	if (info.markers.clustered_marker_indices.includes(index)) {
-		var elements = info.markers.clustered_marker_dict[index].elements;
-		for (let i=0; i < elements.length; i++) {
-			var el= elements[i];
-			var information = info.markers.d[el.options.index];
-			show_city(information)
-		}
-        info.right_sidebar_elements = info.markers.clustered_marker_dict[index].elements;
-	} else {
-        info.right_sidebar_elements = false;
-		var information =info.markers.d[index];
-		show_city(information)
-	}
-    info.right_sidebar_active = true;
-    info.right_sidebar_index = index;
-    console.log('rsi',info.right_sidebar_index)
-}
-
-function update_right_sidebar() {
-    if (info.right_sidebar_active === false || info.right_sidebar_index === false) {
-        // if right sidebar is not active do not update it
-        console.log('right sidebar not active, doing nothing')
-        return;
-     }
-    if (info.right_sidebar_elements === false) {
-        // only one city linked to this marker showing info related to that city
-        console.log('updating sidebar')
-        show_right_sidebar(info.right_sidebar_index);    
-        return;
-    }
-    // multiple cities linked to marker, showing info related to each city
-    //console.log(info.right_sidebar_elements)
-    var indices = []
-    for (let i = 0; i < info.right_sidebar_elements.length; i++) {
-        var el = info.right_sidebar_elements[i]
-        if (info.markers.clustered_marker_indices.includes(el.options.index)) {
-            show_right_sidebar(el.options.index) 
-            return
-        } else {
-            indices.push(el.options.index)
-        }
-    }
-    for (let i = 0; i < indices.length; i++) {
-        var index = indices[i];
-        if ( info.markers.d[index] != undefined && info.markers.d[index].count > 0) { 
-            show_right_sidebar(index);
-            return;
-        }
-    }
-    console.log('could not update right sidebar, closing')
-    close_right_sidebar();
-}
 
 function toggle_sidebar_category(element) {
 	//hide reveal category items (e.g. Text) from sidebar
@@ -407,18 +272,6 @@ function toggle_sidebar_category(element) {
 	}
 }
 
-info.markers.update_markers(info.markers.active_markers);
-
-info.map_info.map.on('zoomend', function() {
-	// update the clustering after zooming in or out
-	console.log('zoomed')
-    if (info.connection_view) {var marker_types = 'connection_view'}
-    else {var marker_types = 'overview'}
-    var all_markers = info.markers.layerDict[marker_types];
-	info.markers.update_markers(all_markers, info.map_info.map);
-});
-
-open_left_sidebar();
 
 function update_category_headers() {
 	var category_headers = document.getElementsByClassName('category-header');
@@ -450,11 +303,25 @@ function toggle_filter(name) {
     filter_toggle_filter(name,info);
 }
 
+
+info.markers.update_markers(info.markers.active_markers);
+
+info.map_info.map.on('zoomend', function() {
+	// update the clustering after zooming in or out
+	console.log('zoomed')
+    if (info.connection_view) {var marker_types = 'connection_view'}
+    else {var marker_types = 'overview'}
+    var all_markers = info.markers.layerDict[marker_types];
+	info.markers.update_markers(all_markers, info.map_info.map);
+});
+
+sidebar.open_left_sidebar();
+
 window.toggle_filter = toggle_filter;
 window.on_marker_click = on_marker_click;
 window.on_marker_hover = on_marker_hover;
 window.info = info;
 window.sidebar = sidebar;
 
-export {show_info, show_right_sidebar, update_right_sidebar}
+export {show_info}
 
